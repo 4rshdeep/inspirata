8a4811e12c06449e5458df0a837feaac35addaf6
Siddhant24 <sidbhakar@gmail.com>
2018-01-27 23:41:40 +0530

    testing fb

--- facebookbot/bot.py
+++ facebookbot/bot.py
@@ -1,8 +1,8 @@
 import facebook
 import os
 
-app_id = os.environ['APP_ID']
-app_secret = os.environ['APP_SECRET']
+app_id = "340963926402035"
+app_secret = "b9c46f67e14f5d69c343e0df06bea0bd"
 
 graph = facebook.GraphAPI(access_token='EAACEdEose0cBADJY4EEZCKw86CA5LFt5NXelh7weB0F6EHKVodV6Jb2Vt6nmtuqZB2ZCwCuf486Gsfw0rcJruSLZARlaJlH7I52mIYanPJPgyiFZBt9acFr9AQCdktKt99R52inH9cG1sMyZA5MvuOZAcxwfUF1P3Q0VUiBvmhzeJlIrFNsEV9BuMsGfHE1wBvUy2SkYZBiBOgZDZD', version="2.10")
 # get_app_access_token(app_id, app_secret)
@@ -11,3 +11,8 @@ users = graph.search(type='user', q='4rshdeep')
 
 for user in users['data']:
     print('%s %s' % (user['id'],user['name'].encode()))
+
+
+
+
+graph.put_comment(object_id='531752773523381_1767214209977225', message='Great post...')
